eb0439e7b2a8c1d7c3e56689e7c2d779364191f2
Mike Sadler <101577043+mtsadler-branch@users.noreply.github.com>
2022-08-11 15:04:31 -0500

    set results

--- helpers/run_sql_checks.py
+++ helpers/run_sql_checks.py
@@ -52,6 +52,7 @@ def parse_manifest(manifest_file, files, ignored_schemas, dev_prefix):
 
 
 def run_checks(models, sql_checks_path, dev_prefix, prod_prefix, fallback_prefix, irregular_schemas):
+    results = {}
     env = jinja2.Environment(loader=jinja2.FileSystemLoader(sql_checks_path))
     for file in os.listdir(sql_checks_path):
         template = env.get_template(file)
