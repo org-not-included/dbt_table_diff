44d207c0e9e2c976b68d06eac9b1c906a5461ab7
Mike Sadler <101577043+mtsadler-branch@users.noreply.github.com>
2022-09-05 11:14:14 -0500

    bump version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from pathlib import Path
 
 
-VERSION = '2.2.0'
+VERSION = '2.2.1'
 DESCRIPTION = 'Compares models in dbt during an open PR'
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
